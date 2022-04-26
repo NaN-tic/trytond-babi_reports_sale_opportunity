520cf018d5aa323fcd8b5a687c73059696e56d1a
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:16:07 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.babi_reports_sale_opportunity.tests.test_babi_reports_sale_opportunity import suite
-except ImportError:
-    from .test_babi_reports_sale_opportunity import suite
-
-__all__ = ['suite']
